c7f60b5fe8da10b085add140e154086d8bc8c25e
jin <88445014+ShengjieJin@users.noreply.github.com>
2024-10-09 20:01:28 +0800

    Update start_clash.sh
    脚本中添加环境变量

--- start_clash.sh
+++ start_clash.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+export HTTP_PROXY=http://127.0.0.1:7890
+export HTTPS_PROXY=http://127.0.0.1:7890
+export NO_PROXY=localhost,127.0.0.1
 tmux new-session -d -s clash './bin/clash-linux-amd64 -d ./conf 2>&1 | tee -a ./logs/myclash.log'
 
